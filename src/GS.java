2374abc8e90bbae96693a920e2addf75929bb064
Unknown <guerrered8@gmail.com>
2017-02-06 22:42:51 -0600

    algorithm updates

--- src/GS.java
+++ src/GS.java
@@ -7,11 +7,11 @@ public class GS {
 		Printer(mPref, 10);
 		System.out.println("\nWomen's: \n");
 		Printer(wPref, 10);
-	/*
+	
 		int[][] Matching = algorithm(mPref, wPref, 10);
 		System.out.println("The stable matching is : \n");
 		matchPrinter(Matching, 10);
-	*/
+	
 	}
 	
 	public static int[][] algorithm(int[][] mList, int[][] wList, int n){
@@ -25,19 +25,68 @@ public class GS {
 			}
 			i++;
 		}
-		int Full = 0;//int tied to matchlist
-		while( Full < n){
-			int m = 1;//starting with man 1
-			i = 0;//find the first single man
-			if(m == match[i][0]){
-				m++;
-				i++;
-			}
+		int[] currentMatchesM = new int[n];
+		i = 0;
+		while(i < n){//cM index is women values at index is her match
+			currentMatchesM[i] = 0;
+			i++;
+		}
+		int[] currentMatchesW = new int[n];
+		i = 0;
+		while(i < n){//cM index is women values at index is her match
+			currentMatchesW[i] = 0;
+			i++;
 		}
 		
-	
+		
+		int male = 1;
+		while(male < n){
+			int nextChoice = currentMatchesM[male-1];
+			int woman = mList[male-1][nextChoice];//return 1-10
+			if(currentMatchesW[woman-1] == 0){//no pair woman accepts first suitor
+				i =0;
+				while(i < n){//find the index of the man from woman POV
+					if(wList[woman-1][i] == male){
+						currentMatchesW[woman-1] = i;
+						currentMatchesM[male-1] = nextChoice;
+						male++;//go to next guy
+						break;
+					}
+					i++;
+				}
+			}
+			else{//woman already matched
+				i =0;
+				while(i < n){//find the index of the new man from woman POV
+					if(wList[woman-1][i] == male){
+						break;
+					}
+					i++;
+				}
+				if(i < currentMatchesW[woman-1]){//if she likes new guy better match with him
+					currentMatchesM[currentMatchesW[woman-1]]++;//that man will look to next person;
+					currentMatchesW[woman-1] = i;
+					male++;//go to next guy
+				}
+				else{//not liked
+					currentMatchesM[male-1]++;//look to next woman in list
+				}
+			}
+			if(male == n){//check if done no repeats
+				
+			}
+		}
+		i =0;
+		while(i < n){
+			match[i][0] = i + 1;
+			match[i][1] = mList[i][currentMatchesM[i]];
+			
+			i++;
+		}
 		return match;
 	}
+	
+	
 	public static void matchPrinter(int[][] x,int n){
 		int  i = 0;
 		while(i < n){
@@ -50,6 +99,8 @@ public class GS {
 			i++;
 		}
 	}
+	
+	
 	public static int[][] generatePreference(int n){
 		int[][] PList = new int[n][n];
 		boolean[] used = new boolean[n];
@@ -62,7 +113,7 @@ public class GS {
 		while( i < n ){
 			while(j < n ){
 				boolean found = false;
-				int f = (int)(Math.random() * n + 1);
+				int f = (int)(Math.random() * n + 1 );
 				while(!found){
 					if(used[f -1] == true){
 						f = (int)(Math.random() * n + 1);
@@ -84,6 +135,8 @@ public class GS {
 		}
 		return PList;
 	}
+	
+	
 	public static void Printer(int[][] x, int n){
 		int i= 0, j =0;
 		while(i < n){
